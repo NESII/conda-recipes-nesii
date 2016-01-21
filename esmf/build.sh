dcbec8a22d17348c855984f6607da7611bd84285
Ryan.O'Kuinghttons <rokuingh@gmail.com>
2016-01-21 15:36:41 -0700

    add ESMF_INSTALL_BINDIR to make the ESMF apps available after installation

--- esmf/build.sh
+++ esmf/build.sh
@@ -3,6 +3,7 @@
 
 export ESMF_DIR=`pwd`
 export ESMF_INSTALL_PREFIX=${PREFIX}
+export ESMF_INSTALL_BINDIR=${PREFIX}/bin
 export ESMF_NETCDF="split"
 export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
