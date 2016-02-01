7875f1a0eb33ace98530f1c3945e416600ed69ac
bekozi <benkoziol@gmail.com>
2016-02-01 11:23:15 -0700

    Build for ESMF 7.0.0.dev1-1

--- sh/docker/build-esmpy.sh
+++ sh/docker/build-esmpy.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-
-anaconda login
+#anaconda login
 conda config --add channels ${ADD_CHANNEL}
 cd /conda-esmf
 conda build esmpy
-for package in ${TO_UPLOAD[@]}; do
-    upload=`conda build --output ${package}` && \
-    anaconda upload --force -u nesii -c ${UPLOAD_CHANNEL} ${upload}
-done
+#anaconda upload --force -u nesii -c ${UPLOAD_CHANNEL} `conda build --output esmpy esmf`
