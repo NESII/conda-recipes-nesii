b64b0b1dea00577641da6a2158ed3a454414d01a
bekozi <benkoziol@gmail.com>
2016-02-02 17:17:14 -0700

    New build for ESMF
    - Incremented ESMF builds for new build parameters.
    - Removed OCGIS and ICCLIM.
    - Renamed docker files.
    - Fixed Travis testing.

--- sh/docker/build-esmpy.sh
+++ sh/docker/build-esmpy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#anaconda login
-conda config --add channels ${ADD_CHANNEL}
 cd /conda-esmf
+conda config --add channels nesii
+conda build esmf
 conda build esmpy
-#anaconda upload --force -u nesii -c ${UPLOAD_CHANNEL} `conda build --output esmpy esmf`
-#anaconda upload --force -u nesii -c esmf `conda build --output esmpy esmf`
+#anaconda login
+#anaconda upload -u nesii -c esmf `conda build --output esmpy esmf`
--- sh/docker/build.sh
+++ sh/docker/build.sh
@@ -3,23 +3,6 @@
 
 DIMAGE=bekozi/nbuild-centos6
 
-#BUILD_TARGET=ocgis
-BUILD_TARGET=esmpy
-
-ADD_CHANNEL=nesii/channel/esmf
-
-#CBUILD_ESMF_VERSION=6.3.0rp1
-#CBUILD_ESMF_TAG=ESMF_6_3_0rp1
-CBUILD_ESMF_VERSION=7.0.0.dev1
-CBUILD_ESMF_TAG=ESMF_7_0_0
-
-CBUILD_OCGIS_VERSION=1.2.0.dev1
-CBUILD_OCGIS_TAG=next
-
-#UPLOAD_CHANNEL=dev-ocgis
-#UPLOAD_CHANNEL=dev-esmf-no-gcc
-UPLOAD_CHANNEL=dev
-
 ########################################################################################################################
 ## BUILD DOCKER IMAGE ##
 
@@ -28,10 +11,4 @@ UPLOAD_CHANNEL=dev
 
 ########################################################################################################################
 ## BUILD/UPLOAD ESMPy ##
-
-ENV="-e UPLOAD_CHANNEL=${UPLOAD_CHANNEL} -e CBUILD_ESMF_VERSION=${CBUILD_ESMF_VERSION} \
-     -e CBUILD_ESMF_TAG=${CBUILD_ESMF_TAG} -e CBUILD_OCGIS_VERSION=${CBUILD_OCGIS_VERSION} \
-     -e CBUILD_OCGIS_TAG=${CBUILD_OCGIS_TAG} -e ADD_CHANNEL=${ADD_CHANNEL}"
-docker run ${ENV} --cpuset-cpus=3 -it -v ~/l/project/conda-esmf:/conda-esmf ${DIMAGE} bash
-#docker run ${ENV} --cpuset-cpus=3 -it -v ~/l/project/conda-esmf:/conda-esmf ${DIMAGE} \
-#    bash /conda-esmf/sh/docker/build-${BUILD_TARGET}.sh
+docker run --cpuset-cpus=3 -it -v ~/l/project/conda-esmf:/conda-esmf ${DIMAGE} bash
