6042a548f2348ec7993d51c696318f596ca26b68
bekozi <benkoziol@gmail.com>
2016-03-07 15:01:53 -0700

    Added krb5 dependency to esmf

--- sh/docker/docker-image-build.sh
+++ sh/docker/docker-image-build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 
-pushd /home/ubuntu/project/ocg/git/ocgis/conda-recipes
+pushd ~/l/project/conda-esmf
 
-#name=bekozi/nbuild-centos6
-#file=./Dockerfile-CentOS6-Builder
-#docker build -t ${name} --file ${file} .
-#docker push ${name}
-
-name=bekozi/ntest-ubuntu
-file=./Dockerfile-Ubuntu-Tester
-docker build -t ${name} --file ${file} .
+name=bekozi/nbuild-centos6
+file=./Dockerfile-CentOS6-Builder
+docker build --no-cache -t ${name} --file ${file} .
 docker push ${name}
+
+#name=bekozi/ntest-ubuntu
+#file=./Dockerfile-Ubuntu-Tester
+#docker build --no-cache -t ${name} --file ${file} .
+#docker push ${name}
 
 popd
--- sh/docker/docker-test-packages.sh
+++ sh/docker/docker-test-packages.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-docker pull bekozi/ntest-ubuntu
-docker rm ocgis-test-runner
-docker run -id --name ocgis-test-runner bekozi/ntest-ubuntu
-docker exec ocgis-test-runner bash -c "conda create -y -n ocgis -c nesii ocgis nose esmpy"
-docker exec ocgis-test-runner bash -c "source activate ocgis && python -c \"from ocgis.test import run_all; run_all()\""
-docker stop ocgis-test-runner
+image=bekozi/ntest-ubuntu
+name=ocgis-test-runner
+
+docker pull ${image}
+docker rm ${name}
+docker run -id --name ${name} ${image}
+docker exec ${name} bash -c "conda create -y -n test-ocgis -c nesii ocgis nose esmpy"
+docker exec ${name} bash -c "source activate test-ocgis && python -c \"from ocgis.test import run_all; run_all()\""
+docker stop ${name}
