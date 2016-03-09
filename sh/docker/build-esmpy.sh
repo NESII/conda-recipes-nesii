f12c5f60cc0d0ef31eb9b7f18c9fad8ea56f19cf
bekozi <benkoziol@gmail.com>
2016-03-09 12:25:46 -0700

    New HEAD build

--- sh/docker/build-esmpy.sh
+++ sh/docker/build-esmpy.sh
@@ -4,4 +4,5 @@ cd /conda-esmf
 conda build -c nesii esmf
 conda build -c nesii esmpy
 #anaconda login
-#anaconda upload -u nesii `conda build --output esmpy esmf`
+#anaconda upload -u nesii -c esmf -c main `conda build --output esmpy esmf`
+#anaconda upload -u nesii -c esmf -c main `conda build --output esmf`
