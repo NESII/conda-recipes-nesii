f2462b06135dacf45cb45573aeaeae6e0405810a
bekozi <benkoziol@gmail.com>
2016-07-26 14:19:14 -0600

    BLD: Incremented GDAL number to account for GEOS version

--- sh/docker-image-build.sh
+++ sh/docker-image-build.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 
+#docker login
+
 pushd ~/l/project/conda-esmf
 
 name=bekozi/nbuild-centos6
 file=./Dockerfile-CentOS6-Builder
-docker build --no-cache -t ${name} --file ${file} .
+docker build --pull --no-cache -t ${name} --file ${file} .
 docker push ${name}
 
 name=bekozi/ntest-ubuntu
 file=./Dockerfile-Ubuntu-Tester
-docker build --no-cache -t ${name} --file ${file} .
+docker build --pull --no-cache -t ${name} --file ${file} .
 docker push ${name}
 
 popd
