9c30715bd63767de2187f99a92da43349179bf7d
bekozi <benkoziol@gmail.com>
2016-08-01 08:56:12 -0600

    BLD: Added conda build netcdf-fortran to shell script

--- sh/build-esmpy-from-github-repo.sh
+++ sh/build-esmpy-from-github-repo.sh
@@ -11,7 +11,8 @@ conda install --yes conda-build
 cd ${WD}
 git clone -b ${BRANCH} https://github.com/NESII/conda-recipes-nesii.git
 cd conda-recipes-nesii
-conda build -c conda-forge esmpy
+conda build netcdf-fortran
+conda build esmpy
 conda create --yes -n ${ENV_NAME} --use-local esmpy==HEAD
 source activate ${ENV_NAME}
 python -c "import ESMF; print('ESMF import successful.')"
