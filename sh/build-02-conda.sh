6039351c5cdf11b2bb9aea3c1df1851b5b275709
bekozi <benkoziol@gmail.com>
2017-06-21 12:24:06 -0600

    ESMF head build

--- sh/build-02-conda.sh
+++ sh/build-02-conda.sh
@@ -2,7 +2,8 @@
 
 LOGDIR=/project/conda-recipes-nesii/logs
 
-ESMPY_PYVER=(2.7 3.5 3.6)
+ESMPY_PYVER=(3.5)
+#ESMPY_PYVER=(2.7 3.5 3.6)
 OCGIS_PYVER=(2.7 3.5 3.6)
 ICCLIM_PYVER=(2.7)
 
@@ -15,8 +16,8 @@ OCGIS_LABEL="-l main -l ocgis"
 #FORCE="false"
 FORCE="true"
 
-UPLOAD="true"
-#UPLOAD="false"
+#UPLOAD="true"
+UPLOAD="false"
 
 BUILD="true"
 #BUILD="false"
@@ -48,26 +49,26 @@ fi
 #    anaconda upload -u nesii ${FORCE_FLAG} ${ESMF_LABEL} `conda build --output esmf`
 #fi
 
-#for ii in ${ESMPY_PYVER[*]}; do
-#    if [ ${BUILD} == "true" ]; then
-#        conda build -c conda-forge --python ${ii} esmpy 2>&1 | tee ${LOGDIR}/cb-esmpy-py${ii}-`date +%Y%m%d-%H%M%S`.log
-#    fi
-#    if [ ${UPLOAD} == "true" ]; then
-#        anaconda upload -u nesii ${FORCE_FLAG} ${ESMF_LABEL} `conda build --output --python ${ii} esmpy`
-#    fi
-#done
+for ii in ${ESMPY_PYVER[*]}; do
+    if [ ${BUILD} == "true" ]; then
+        conda build -c conda-forge --python ${ii} esmpy 2>&1 | tee ${LOGDIR}/cb-esmpy-py${ii}-`date +%Y%m%d-%H%M%S`.log
+    fi
+    if [ ${UPLOAD} == "true" ]; then
+        anaconda upload -u nesii ${FORCE_FLAG} ${ESMF_LABEL} `conda build --output --python ${ii} esmpy`
+    fi
+done
 
 #######################################################################################################################
 # Build OCGIS
 
-for ii in ${OCGIS_PYVER[*]}; do
-    if [ ${BUILD} == "true" ]; then
-        conda build -c conda-forge --python ${ii} ocgis
-    fi
-    if [ ${UPLOAD} == "true" ]; then
-        anaconda upload -u nesii ${FORCE_FLAG} ${OCGIS_LABEL} `conda build --output --python ${ii} ocgis`
-    fi
-done
+#for ii in ${OCGIS_PYVER[*]}; do
+#    if [ ${BUILD} == "true" ]; then
+#        conda build -c conda-forge --python ${ii} ocgis
+#    fi
+#    if [ ${UPLOAD} == "true" ]; then
+#        anaconda upload -u nesii ${FORCE_FLAG} ${OCGIS_LABEL} `conda build --output --python ${ii} ocgis`
+#    fi
+#done
 
 ########################################################################################################################
 # Build ICCLIM
